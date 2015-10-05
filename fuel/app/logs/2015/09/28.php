3d870f297b3685b67e94967e323686df337c29c0
khoapld <pkhoa@yahoo-corp.jp>
2015-10-05 09:37:23 +0700

    update source

--- fuel/app/logs/2015/09/28.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2015-09-28 11:00:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2015-09-28 11:01:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
